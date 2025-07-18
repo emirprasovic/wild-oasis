5474cf4dc211556e491fd924bcaacac0608dcdc4
emirprasovic <eemaemir@gmail.com>
2025-07-18 18:48:27 +0200

    Account page and dark mode implemented

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -55,4 +55,49 @@ export async function logout() {
   }
 
   return error?.message;
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1) Update password or fullname
+  let updateData;
+  if (password) {
+    updateData = { password };
+  } else if (fullName) {
+    updateData = { data: { fullName } };
+  }
+
+  let { data, error } = await supabase.auth.updateUser(updateData);
+  if (!avatar) return data;
+
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+
+  // 2) Upload avatar if specified
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: uploadError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (uploadError) {
+    console.error(uploadError);
+    throw new Error(uploadError.message);
+  }
+
+  // 3) Update avatar in user
+  const { data: updatedUser, error: updateError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (updateError) {
+    console.error(updateError);
+    throw new Error(updateError.message);
+  }
+
+  return updatedUser;
 }
